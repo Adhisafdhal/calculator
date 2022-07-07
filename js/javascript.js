49f8f6aa178a6780640c4990dcde52ecf8219186
Aadheuss <aadheusx@gmail.com>
2022-07-07 16:25:48 +0700

    Add delete function to input

--- js/javascript.js
+++ js/javascript.js
@@ -20,15 +20,26 @@ function removeTransition (e) {
 buttons.forEach(button => button.addEventListener('transitionend', removeTransition))
 //remove transition effect
 
+function changeFontSize() {
+  if (output.innerText.length < 12) {
+    output.style.fontSize = '2rem';
+  } else {
+    output.style.fontSize = '1.6rem';
+  };
+}
+
 function input(e) {
   const target = e.target.innerText;
   if (/[0-9]/.test(target)) {
-    if (output.innerText.length < 13) {
-      output.innerText = output.innerText + target;
-    }
+    if (output.innerText.length < 15) {
+    output.innerText = output.innerText + target;
+    };
+  } else if (e.target.dataset.name = 'delete') {
+    output.innerText = output.innerText.slice(0, - 1);
   }
 
   transition(e);
+  changeFontSize();
 }
 
 function add(a, b) {
